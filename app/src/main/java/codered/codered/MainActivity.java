7b27d044fb8a597790259a7010ca2a8876dfded1
Kailey1 <chen.kailey1@gmail.com>
2020-06-19 18:06:00 -0400

    nav bar changed color

--- app/src/main/java/codered/codered/MainActivity.java
+++ app/src/main/java/codered/codered/MainActivity.java
@@ -27,9 +27,9 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
+                /*case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
-                    return true;
+                    return true;*/
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
