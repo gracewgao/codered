b37b981615403ad7981cfe4302c05891f890b45c
gracewgao <ggaoww@gmail.com>
2020-06-19 19:52:01 -0400

    added the request detail connect to firebase?

--- app/src/main/java/codered/codered/RequestActivity.java
+++ app/src/main/java/codered/codered/RequestActivity.java
@@ -1,12 +1,16 @@
 package codered.codered;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -47,8 +51,22 @@ public class RequestActivity extends AppCompatActivity {
         int product = productSpinner.getSelectedItemPosition();
         // creates new request object
         Request r = new Request(rId, product, message);
-        requestRef.child(rId).setValue(r);
+        requestRef.child(rId).setValue(r)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // successfully saved
+                        Toast.makeText(getApplicationContext(),"Your request has been sent!",Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // failed to save
+                        Toast.makeText(getApplicationContext(),"Uh-oh! something went wrong, please try again.",Toast.LENGTH_SHORT).show();
+                    }
+                });;
 
-        finish();
     }
 }
