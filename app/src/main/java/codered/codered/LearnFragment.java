e0aba12805a92a5225ba2cbd333e4d20eeec3b19
gracewgao <ggaoww@gmail.com>
2020-06-20 15:28:57 -0400

    made a safety page

--- app/src/main/java/codered/codered/LearnFragment.java
+++ app/src/main/java/codered/codered/LearnFragment.java
@@ -1,17 +1,34 @@
 package codered.codered;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class LearnFragment extends Fragment {
+
+    private CardView safetyCard;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_learn, null);
+        View v = inflater.inflate(R.layout.fragment_learn, null);
+        safetyCard = v.findViewById(R.id.safety_card);
+        safetyCard.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), SafetyActivity.class);
+                getActivity().startActivity(i);
+            }
+        });
+
+        return v;
+
     }
 }
