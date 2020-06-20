40d16067034a3860642cb2f83ab79ae00d229edd
Bonnie Chin <bonniechin.mail@gmail.com>
2020-06-20 19:51:42 -0400

    files made!

--- app/src/main/java/codered/codered/ProductsActivity.java
+++ app/src/main/java/codered/codered/ProductsActivity.java
@@ -1,14 +1,13 @@
 package codered.codered;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class SafetyActivity extends AppCompatActivity {
+public class ProductsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_safety);
+        setContentView(R.layout.activity_products);
     }
 }
-
