4ef08712395e45c85145de1413c041a5f2d4eb11
gracewgao <ggaoww@gmail.com>
2020-06-20 02:06:55 -0400

    measures distance away from user

--- app/src/main/java/codered/codered/MainActivity.java
+++ app/src/main/java/codered/codered/MainActivity.java
@@ -1,9 +1,13 @@
 package codered.codered;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.MenuItem;
@@ -11,6 +15,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -19,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
     // arrays to translate saved index into a string
     public static String[] products = {"Tampon", "Pad", "Painkiller"};
     public static String[] states = {"Pending", "Answered", "Cancelled"};
+
+    private final int REQUEST_ACCESS_FINE_LOCATION=1;
+    private FusedLocationProviderClient fusedLocationClient;
+    private double lat, lng;
+
+    private Location location;
 
     // Views
     private TextView mTextMessage;
@@ -33,9 +46,6 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                /*case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;*/
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
@@ -50,6 +60,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
@@ -65,17 +78,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
         detailCard = findViewById(R.id.card1);
         detailCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, RequestDetailActivity.class);
 //                i.putExtra("RID", r.getId());
-                i.putExtra("RID", "-MADgB0kTVdXQnM615tX");
+                i.putExtra("RID", "-MAEqNgPIR7E6SIuVxij");
                 MainActivity.this.startActivity(i);
             }
         });
 
+        // gets user's current location
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location l) {
+                            if (l != null) {
+                                location = l;
+                            }
+                        }
+                    });
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_ACCESS_FINE_LOCATION);
+        }
+
+    }
+
+    public static int findDistance(Location l, double targetLat, double targetLng){
+        Location target = new Location("target");
+        target.setLatitude(targetLat);
+        target.setLongitude(targetLng);
+        int d = (int)Math.floor(l.distanceTo(target));
+        return d;
     }
 
 }
--- app/src/main/java/codered/codered/RequestDetailActivity.java
+++ app/src/main/java/codered/codered/RequestDetailActivity.java
@@ -1,10 +1,17 @@
 package codered.codered;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,25 +24,49 @@ import static codered.codered.MainActivity.states;
 public class RequestDetailActivity extends AppCompatActivity {
 
     private static final String TAG = "RequestDetailActivity";
-    private TextView timeText, messageText, productText, statusText;
+    private TextView timeText, messageText, productText, statusText, distanceText, codeText;
     private String rId;
 
     // Firebase
     DatabaseReference fireRef = FirebaseDatabase.getInstance().getReference();
 
+    private FusedLocationProviderClient fusedLocationClient;
+    private Location location;
+
+    private final int REQUEST_ACCESS_FINE_LOCATION=1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_request_detail);
 
         timeText = findViewById(R.id.read_time);
         messageText = findViewById(R.id.read_message);
         productText = findViewById(R.id.read_product);
         statusText = findViewById(R.id.read_status);
+        distanceText = findViewById(R.id.read_location);
+        codeText = findViewById(R.id.read_code);
 
         Bundle extras = getIntent().getExtras();
         if(extras != null) {
             rId = extras.getString("RID");
+        }
+
+        // gets user's current location
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location l) {
+                            if (l != null) {
+                                location = l;
+                            }
+                        }
+                    });
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_ACCESS_FINE_LOCATION);
         }
 
         fireRef.child("requests").child(rId).addValueEventListener(new ValueEventListener() {
@@ -47,6 +78,9 @@ public class RequestDetailActivity extends AppCompatActivity {
                 statusText.setText(states[r.getStatus()]);
                 String time = Request.convertTime((long)r.getTimestamp());
                 timeText.setText(time);
+                String distance = MainActivity.findDistance(location, r.getLat(), r.getLng())+ " m away";
+                distanceText.setText(distance);
+                codeText.setText(r.getCode());
             }
 
             @Override
