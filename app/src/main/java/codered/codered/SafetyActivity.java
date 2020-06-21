5734847bcab21cd452b9c1f1c867b6cd6d9ed69a
Bonnie Chin <bonniechin.mail@gmail.com>
2020-06-20 22:23:00 -0400

    BUTTON LEAVES YESYESYES

--- app/src/main/java/codered/codered/SafetyActivity.java
+++ app/src/main/java/codered/codered/SafetyActivity.java
@@ -1,14 +1,28 @@
 package codered.codered;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SafetyActivity extends AppCompatActivity {
+
+    ImageButton myImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_safety);
+
+        myImageButton = (ImageButton) findViewById(R.id.my_image_button);
+        myImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadNewActivity = new Intent (SafetyActivity.this, LearnFragment.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
     }
 }
 
